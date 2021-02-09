6e4d2897456dca0c0d632f2e4a528a8c32081e22
Anny Cárdenas <ajcardenasb1@gmail.com>
2021-02-09 17:58:41 +0100

    Update RSS_CBASS_permanovas.R

--- RSS_CBASS_permanovas.R
+++ RSS_CBASS_permanovas.R
@@ -6,23 +6,22 @@ setwd("~/Documents/Bioinformatics_scripts/R_scripts/RSS_16S/ASVs/")
 map=read.table("Input_files/RSS_metadata.txt", header = T, row.names = 1, sep = "\t")
 asv=read.table("Input_files/RSSvsCBASS_ASV_table_pooled.txt", header = TRUE, row.names = 1)[,1:68]
 
-#asv_clr=as.data.frame(t(apply(asv,2,clr)))
-asv_trn=as.data.frame(t(sweep(asv,2,colSums(asv),"/")))
-rownames(asv_trn)=gsub('\\.', '-', rownames(asv_trn))
-asv_trn$groups=paste(map$Experiment, map$Time, map$Temperature, sep = "_")[match(rownames(asv_trn), rownames(map))]
-asv_trn$Temperature=map$Temperature[match(rownames(asv_trn), rownames(map))]
-asv_trn$Time=map$Time[match(rownames(asv_trn), rownames(map))]
+#clr trasnformation
+asv_clr=as.data.frame(t(apply(asv,2,clr)))
+rownames(asv_clr)=gsub('\\.', '-', rownames(asv_clr))
+asv_clr$groups=paste(map$Experiment, map$Time, map$Temperature, sep = "_")[match(rownames(asv_clr), rownames(map))]
+asv_clr$Temperature=map$Temperature[match(rownames(asv_clr), rownames(map))]
+asv_clr$Time=map$Time[match(rownames(asv_clr), rownames(map))]
+asv_clr$Genotype=map$Genotype[match(rownames(asv_clr), rownames(map))]
 
 ## CB
-asv.acu=subset(asv_trn, rownames(asv_trn) %like% "^CB")
-adonis(asv.acu[,1:8660] ~ asv.acu$Temperature + asv.acu$Time)
-acu.pai=pairwise.adonis(asv.acu[,1:8660], asv.acu$groups, p.adjust.m ='fdr', sim.method = 'bray')
+asv.acu=subset(asv_clr, rownames(asv_clr) %like% "^CB")
+adonis(asv.acu[,1:8660] ~ asv.acu$Temperature + asv.acu$Time +  asv.acu$Genotype, method = "euclidean")
+pairwise.adonis(asv.acu[,1:8660], asv.acu$groups, p.adjust.m ='fdr', sim.method = 'euclidean')
 
 ## RSS
-asv.chr=subset(asv_trn, rownames(asv_trn) %like% "^RSS")
-adonis(asv.chr[,1:8660] ~ asv.chr$Temperature + asv.chr$Time)
-chr.pai=pairwise.adonis(asv.chr[,1:8660], asv.chr$groups, p.adjust.m ='fdr', sim.method = 'bray')
-
-
+asv.chr=subset(asv_clr, rownames(asv_clr) %like% "^RSS")
+adonis(asv.chr[,1:8660] ~ asv.chr$Temperature + asv.chr$Time + asv.chr$Genotype,  method = "euclidean")
+pairwise.adonis(asv.chr[,1:8660], asv.chr$groups, p.adjust.m ='fdr', sim.method = 'euclidean')
 
 
